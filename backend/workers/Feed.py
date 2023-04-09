cf636824fb5a5dadf8c65a2b22e342203bb52028
Efasquel <64380933+Efasquel@users.noreply.github.com>
2023-04-09 11:21:24 +0200

    Add a worker to get RSS feed and oarse the data in the feed: * Feed.py contains the Feed object and methods to parse a feed. * workerRSS.py contains the methods to call the parser and retrieve the data in a json format.

---
+++ backend/workers/Feed.py
@@ -0,0 +1,108 @@
+import json
+from datetime import datetime
+
+class Feed:
+    data = {
+        "status": 0,
+        "error": 0,
+        "source": {},
+        "articles": []
+    }
+    def __init__(self, status:int, id:int=None):
+        self.id = id
+        self.data["status"] = status
+        
+    def serialize(self) -> str:
+        return json.dumps(self.data)
+    
+    def set_source(self, link:str, title:str, etag:str, description:str, version:str, updatedAt:str=None):
+        self.data["source"] = {
+            "title": title,
+            "description": description,
+        }
+        if version:
+            self.data["source"]["version"] = version
+        if etag:
+            self.data["source"]["etag"] = etag
+        if updatedAt:
+            self.data["source"]["updatedAt"] = updatedAt
+    
+    def add_article(self, id:int, link:str, title:str, description:str, publishedAt:str, updatedAt:str):
+        self.data["articles"].append({
+            "id": id,
+            "link": link,
+            "title": title,
+            "description": description,
+        })
+        if updatedAt:
+            self.data["articles"][-1]["updatedAt"] = updatedAt
+        if publishedAt:
+            self.data["articles"][-1]["publishedAt"] = publishedAt
+
+    def add_link(self, link:str):
+        self.data['source']['link'] = link
+
+    @staticmethod
+    def format_datetime(parsed_date:str) -> str:
+        dt_obj = datetime(*parsed_date[:6])
+        return dt_obj.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+    
+    def scrap_data(self, raw_data) -> int:
+         # Invalid RSS
+        if raw_data.bozo:
+            self.data["error"] = 1
+            return 1
+        # Source title
+        self.data["source"]["title"] = raw_data.feed['title']
+
+        # Source description
+        if "summary" in raw_data.feed:
+            self.data["source"]["description"] = raw_data.feed['summary']
+        elif "subtitle" in raw_data.feed:
+            self.data["source"]["description"] = raw_data.feed['subtitle']
+
+        # Source etag
+        if "etag" in raw_data:
+            self.data["source"]["etag"] = raw_data.etag
+
+        # Source link
+        self.data["source"]["link"] = raw_data.href
+
+        # Source version
+        if "version" in raw_data:
+            self.data["source"]["version"] = raw_data.version
+
+        if "entries" in raw_data:
+            entries = raw_data["entries"]
+            n = len(entries)
+            for i in range(n):
+
+                # Entry id
+                entry_id = entries[i].id
+
+                # Entry link
+                entry_link = entries[i].link
+            
+                # Entry title
+                entry_title = entries[i].title
+
+                # Entry description
+                if "summary" in entries[i]:
+                    entry_description = entries[i]["summary"]
+                elif "description" in entries[i]:
+                    entry_description = entries[i]["description"]
+                else:
+                    entry_description = ""
+
+                # Entry published date and format the datetime object as a string
+                entry_published = ""
+                if "published_parsed" in entries[i]:
+                    entry_published = Feed.format_datetime(entries[i].published_parsed)
+                
+                entry_updated = ""
+                if "updated_parsed" in entries[i]:
+                    entry_updated = Feed.format_datetime(entries[i].updated_parsed)
+
+                self.add_article(id=entry_id, link=entry_link, title=entry_title, description=entry_description, 
+                                publishedAt=entry_published, updatedAt=entry_updated)
+        return 0
--- backend/workers/workerRSS.py
+++ backend/workers/workerRSS.py
@@ -1,17 +1,71 @@
-import json
+# usage: WorkerRSS.py [-h] -l LINK [-d DATE] [-e ETag]
 
-# Read the JSON file
-data = {
-    "code": 200, 
-    "data": {
-        "url": "/etc", 
-        "etag": "30", 
-        "title": "New l",
-        "description": "description",
-        "updatedAt" : "2001-04-05"
-    }
-}
-# Send the JSON data to stdout
+import feedparser
+from http import HTTPStatus
+import argparse
+from urllib.parse import urlparse
+from re import match
+
+from Feed import Feed
+
+BAD_RSS_FORMAT = -1
+PATTERN_DATE = r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z'
+
+def getFeed(link:str, modified:str, etag:str) -> feedparser:
+    return feedparser.parse(link, modified=modified, etag=etag)
+
+def process(raw_data:feedparser) -> Feed:
+    feed = Feed(raw_data.status)
+    if raw_data.status < 400:
+        if raw_data.status == HTTPStatus.NOT_MODIFIED: # 304
+            pass
+        if raw_data.status == HTTPStatus.MOVED_PERMANENTLY: # 301 New permanent address
+            feed.add_link(raw_data.href)
+            feed.scrap_data(raw_data)
+        if raw_data.status == HTTPStatus.FOUND: # 302 New temporary address
+            feed.scrap_data(raw_data)
+        if raw_data.status == HTTPStatus.OK :# 200
+            feed.scrap_data(raw_data)
+    return feed
+
+def check_url(url:str) -> str:
+    # Parse the URL to ensure it's a valid URL
+    parsed_url = urlparse(url)
+    if not all([parsed_url.scheme, parsed_url.netloc]):
+        raise argparse.ArgumentTypeError("Invalid URL: {}".format(url))
+    return url
+
+def check_date(date:str) -> str:
+    # Parse date to ensure it's a valid format
+    if not match(PATTERN_DATE, date):
+        raise argparse.ArgumentTypeError("Invalid date: {}".format(date))
+    return date
+
+def check_etag(etag:str) -> str:
+    if not etag[0] == '"':
+        etag = '"' + etag
+    if not etag[-1] == '"':
+        etag = etag + '"'
+    return etag
+
+def parse_arg() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument("-l", "--link",          dest="link",          type=check_url, 
+                        metavar="LINK", required=True, help="Link to search.")
+    parser.add_argument("-d", "--last_modified", dest="last_modified", type=check_date, 
+                        metavar="DATE" ,help="Date of last known modification of the feed.")
+    parser.add_argument("-e", "--etag",          dest="etag",          type=check_etag, 
+                        metavar="ETag", help="Entity Tag to identify the client.")
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_arg()
+    data = getFeed(args.link, modified=args.last_modified, etag=args.etag)
+    json = process(data)
+    print(json.serialize())
+
 if __name__ == "__main__":
-    print(json.dumps(data))
-    # print("aaaa")
+    main()
+
